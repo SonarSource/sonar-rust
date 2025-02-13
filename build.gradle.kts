f8002c73e1e64e37eb45d94cfcdd514f5f6e5b93
Tibor Blenessy <saberduck@users.noreply.github.com>
2025-02-13 18:01:40 +0100

    fix build name

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention
+
 plugins {
   id("org.sonarqube") version "6.0.1.5171"
   id("com.jfrog.artifactory") version "5.2.5" apply false
@@ -31,4 +33,41 @@ sonar {
     property("sonar.links.issue", "https://jira.sonarsource.com/projects/SKUNK")
   }
 }
+
+configure<ArtifactoryPluginConvention> {
+  clientConfig.info.buildName = "sonar-rust"
+  clientConfig.info.buildNumber = System.getenv("BUILD_NUMBER")
+  clientConfig.isIncludeEnvVars = true
+  clientConfig.envVarsExcludePatterns =
+    "*password*,*PASSWORD*,*secret*,*MAVEN_CMD_LINE_ARGS*,sun.java.command,*token*,*TOKEN*,*LOGIN*,*login*,*key*,*KEY*,*PASSPHRASE*,*signing*"
+
+  // Define the artifacts to be deployed to https://binaries.sonarsource.com on releases
+  clientConfig.info.addEnvironmentProperty("ARTIFACTS_TO_PUBLISH", "${project.group}:sonar-rust-plugin:jar")
+  // The name of this variable is important because it's used by the delivery process when extracting version from Artifactory build info.
+  clientConfig.info.addEnvironmentProperty("PROJECT_VERSION", version.toString())
+
+  setContextUrl(System.getenv("ARTIFACTORY_URL"))
+  publish {
+    repository {
+      repoKey = System.getenv("ARTIFACTORY_DEPLOY_REPO")
+      username = System.getenv("ARTIFACTORY_DEPLOY_USERNAME")
+      password = System.getenv("ARTIFACTORY_DEPLOY_PASSWORD")
+    }
+    defaults {
+      setProperties(
+        mapOf(
+          "build.name" to "sonar-rust",
+          "build.number" to project.ext["buildNumber"].toString(),
+          "pr.branch.target" to System.getenv("PULL_REQUEST_BRANCH_TARGET"),
+          "pr.number" to System.getenv("PULL_REQUEST_NUMBER"),
+          "vcs.branch" to System.getenv("GIT_BRANCH"),
+          "vcs.revision" to System.getenv("GIT_COMMIT"),
+          "version" to project.version.toString()
+        )
+      )
+      setPublishIvy(false) // Publish generated Ivy descriptor files to Artifactory (true by default)
+    }
+  }
+}
+
 
