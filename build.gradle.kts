14d7c449aa34b7ef3be0c6a4d4ff6b11b47b72bf
Tibor Blenessy <saberduck@users.noreply.github.com>
2025-02-13 15:18:59 +0100

    Fix artifactory deploy (#10)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,35 @@
-
-
 plugins {
-    id("org.sonarqube") version "6.0.1.5171"
-    id("com.jfrog.artifactory") version "5.2.5" apply false
-    id("maven-publish")
+  id("org.sonarqube") version "6.0.1.5171"
+  id("com.jfrog.artifactory") version "5.2.5" apply false
+  id("maven-publish")
 }
 
 
 
 repositories {
-    val artifactoryUsername = System.getenv("ARTIFACTORY_PRIVATE_USERNAME") ?: project.findProperty("artifactoryUsername")
-    val artifactoryPassword = System.getenv("ARTIFACTORY_PRIVATE_PASSWORD") ?: project.findProperty("artifactoryPassword")
-    if (artifactoryUsername is String && artifactoryPassword is String)
-        maven {
-            url = uri("https://repox.jfrog.io/repox/sonarsource")
-            credentials {
-                username = artifactoryUsername
-                password = artifactoryPassword
-            }
-        }
-    mavenLocal()
-    mavenCentral()
+  val artifactoryUsername = System.getenv("ARTIFACTORY_PRIVATE_USERNAME") ?: project.findProperty("artifactoryUsername")
+  val artifactoryPassword = System.getenv("ARTIFACTORY_PRIVATE_PASSWORD") ?: project.findProperty("artifactoryPassword")
+  if (artifactoryUsername is String && artifactoryPassword is String)
+    maven {
+      url = uri("https://repox.jfrog.io/repox/sonarsource")
+      credentials {
+        username = artifactoryUsername
+        password = artifactoryPassword
+      }
+    }
+  mavenLocal()
+  mavenCentral()
 }
 
 sonar {
-    properties {
-        property("sonar.projectName", "sonar-rust")
-        property("sonar.projectKey", "SonarSource_sonar-rust")
-        property("sonar.organization", "sonarsource")
-        property("sonar.exclusions", "**/build/**/*")
-        property("sonar.links.ci", "https://cirrus-ci.com/github/SonarSource/sonar-rust")
-        property("sonar.links.scm", "https://github.com/SonarSource/sonar-rust")
-        property("sonar.links.issue", "https://jira.sonarsource.com/projects/SKUNK")
-    }
+  properties {
+    property("sonar.projectName", "sonar-rust")
+    property("sonar.projectKey", "SonarSource_sonar-rust")
+    property("sonar.organization", "sonarsource")
+    property("sonar.exclusions", "**/build/**/*")
+    property("sonar.links.ci", "https://cirrus-ci.com/github/SonarSource/sonar-rust")
+    property("sonar.links.scm", "https://github.com/SonarSource/sonar-rust")
+    property("sonar.links.issue", "https://jira.sonarsource.com/projects/SKUNK")
+  }
 }
 
