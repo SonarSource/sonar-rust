f7487c79813dfee2b0a75b77e81388431f88af4e
Gyula Sallai <gyula.sallai@sonarsource.com>
2025-02-14 14:13:43 +0100

    SKUNK-14 Execute all relevant cargo tasks during build and CI

--- analyzer/build.gradle.kts
+++ analyzer/build.gradle.kts
@@ -1,8 +1,20 @@
 
-
 task<Exec>("compileRust") {
-    description = "Compiles Rust code."
-    inputs.files("src/", "Cargo.toml", "Cargo.lock")
-    outputs.files("target/release/analyzer")
-    commandLine("cargo", "build", "--release")
+  description = "Compiles Rust code."
+  inputs.files("src/", "Cargo.toml", "Cargo.lock")
+  outputs.files("target/release/analyzer")
+  commandLine("cargo", "build", "--release")
+}
+
+task<Exec>("testRust") {
+  description = "Runs Rust tests."
+  inputs.files("src/", "Cargo.toml", "Cargo.lock")
+  outputs.files("target/release/analyzer")
+  commandLine("cargo", "test", "--release")
+}
+
+task<Exec>("checkRustFormat") {
+  description = "Checks Rust code formatting."
+  inputs.files("src/", "Cargo.toml", "Cargo.lock")
+  commandLine("cargo", "fmt", "--", "--check")
 }
