98ae8365be905c1799478118a3a3953fe34f31ad
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2025-02-18 10:41:00 +0100

    Fix quality gate

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,8 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
+rootProject.name = "sonar-rust-plugin"
+
 include("sonar-rust-plugin")
 include("analyzer")
 include("e2e")
