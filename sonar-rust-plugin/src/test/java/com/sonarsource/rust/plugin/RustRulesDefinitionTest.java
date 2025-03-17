bf69472a146364d9860deb34763dd26ccf2c42a2
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2025-03-17 16:14:59 +0100

    SKUNK-48 Add 26 rules for deny Clippy lints and 13 rules for general-purpose Clippy lints (#85)

--- sonar-rust-plugin/src/test/java/com/sonarsource/rust/plugin/RustRulesDefinitionTest.java
+++ sonar-rust-plugin/src/test/java/com/sonarsource/rust/plugin/RustRulesDefinitionTest.java
@@ -22,7 +22,7 @@ class RustRulesDefinitionTest {
   void testClippyRules() {
     var rules = RustRulesDefinition.CLIPPY_RULES;
     assertThat(rules.keySet()).hasSize(new HashSet<>(rules.values()).size());
-    assertThat(rules.keySet()).hasSize(15);
+    assertThat(rules.keySet()).hasSize(54);
     assertThat(rules.keySet()).allSatisfy(ruleKey -> assertThat(ruleKey).startsWith("clippy::"));
   }
 
