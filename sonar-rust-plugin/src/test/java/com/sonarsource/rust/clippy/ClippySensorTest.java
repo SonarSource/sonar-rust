9e6c527de10567e2f69798a9a92950b8c06a25e7
Gyula Sallai <gyula.sallai@sonarsource.com>
2025-03-21 10:41:58 +0100

    SKUNK-56 Record project analysis data with telemetry

--- sonar-rust-plugin/src/test/java/com/sonarsource/rust/clippy/ClippySensorTest.java
+++ sonar-rust-plugin/src/test/java/com/sonarsource/rust/clippy/ClippySensorTest.java
@@ -5,15 +5,6 @@
  */
 package com.sonarsource.rust.clippy;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentCaptor.forClass;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
 import com.sonarsource.rust.cargo.CargoManifestProvider;
 import com.sonarsource.rust.plugin.RustLanguage;
 import java.io.IOException;
@@ -36,6 +27,15 @@ import org.sonar.api.batch.sensor.internal.SensorContextTester;
 import org.sonar.api.batch.sensor.issue.Issue;
 import org.sonar.api.rule.RuleKey;
 import org.sonar.api.testfixtures.log.LogTesterJUnit5;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class ClippySensorTest {
 
@@ -103,7 +103,7 @@ class ClippySensorTest {
     Files.createFile(manifest);
 
     var clippyPrerequisite = mock(ClippyPrerequisite.class);
-    doNothing().when(clippyPrerequisite).check(any());
+    doReturn(new ClippyPrerequisite.ToolVersions("cargo 1.2.3", "clippy 1.2.3")).when(clippyPrerequisite).check(any());
 
     var clippyRunner = mock(ClippyRunner.class);
     doThrow(new IllegalStateException("error")).when(clippyRunner).run(any(), any(), any());
@@ -129,8 +129,7 @@ class ClippySensorTest {
     Files.createFile(manifest);
 
     ClippyPrerequisite clippyPrerequisite = mock(ClippyPrerequisite.class);
-    doNothing().when(clippyPrerequisite).check(any());
-
+    doReturn(new ClippyPrerequisite.ToolVersions("cargo 1.2.3", "clippy 1.2.3")).when(clippyPrerequisite).check(any());
 
     List<ClippyDiagnostic> diagnostics = List.of(
       new ClippyDiagnostic(new ClippyMessage(
@@ -198,7 +197,7 @@ class ClippySensorTest {
     context.fileSystem().add(inputFile);
 
     var clippyPrerequisite = mock(ClippyPrerequisite.class);
-    doNothing().when(clippyPrerequisite).check(any());
+    doReturn(new ClippyPrerequisite.ToolVersions("cargo 1.2.3", "clippy 1.2.3")).when(clippyPrerequisite).check(any());
 
     var clippyRunner = mock(ClippyRunner.class);
     List<ClippyDiagnostic> diagnostics = List.of(
