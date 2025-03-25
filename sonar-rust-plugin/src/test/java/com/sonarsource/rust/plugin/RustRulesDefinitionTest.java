450b85b1acc1cb2c15db86e083aa6f06ddb680b4
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2025-03-25 13:51:54 +0100

    Add rule S2185 (`erasing_op`): Erasing mathematical operations should not be performed

--- sonar-rust-plugin/src/test/java/com/sonarsource/rust/plugin/RustRulesDefinitionTest.java
+++ sonar-rust-plugin/src/test/java/com/sonarsource/rust/plugin/RustRulesDefinitionTest.java
@@ -33,7 +33,7 @@ class RustRulesDefinitionTest {
   void testClippyRules() {
     var rules = RustRulesDefinition.CLIPPY_RULES;
     assertThat(rules.keySet()).hasSize(new HashSet<>(rules.values()).size());
-    assertThat(rules.keySet()).hasSize(67);
+    assertThat(rules.keySet()).hasSize(68);
     assertThat(rules.keySet()).allSatisfy(ruleKey -> assertThat(ruleKey).startsWith("clippy::"));
   }
 
