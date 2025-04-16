e9e28168b6e65b3e74693c99bf15991f35f0946c
Tibor Blenessy <saberduck@users.noreply.github.com>
2025-04-16 10:18:17 +0200

    Update orchestrator to 5.5.0.2535

--- e2e/build.gradle.kts
+++ e2e/build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
     id("java")
 }
 
+val orchestratorVersion = "5.5.0.2535"
+
 dependencies {
     testImplementation("org.assertj:assertj-core:3.26.0")
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.sonarsource.orchestrator:sonar-orchestrator-junit5:5.1.0.2254")
-    testImplementation("org.sonarsource.orchestrator:sonar-orchestrator:5.1.0.2254")
+    testImplementation("org.sonarsource.orchestrator:sonar-orchestrator-junit5:$orchestratorVersion")
+    testImplementation("org.sonarsource.orchestrator:sonar-orchestrator:$orchestratorVersion")
     testImplementation("org.junit.platform:junit-platform-suite-api:1.12.0")
     testImplementation("org.sonarsource.sonarqube:sonar-ws:10.7.0.96327")
 
