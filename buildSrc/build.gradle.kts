082a99e0c8734f83cc844eef5510a0a6db97724f
Tibor Blenessy <saberduck@users.noreply.github.com>
2026-04-27 13:41:08 +0200

    Add credentials to repox repositories in buildSrc and e2e

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,8 +7,16 @@ dependencyLocking {
 }
 
 repositories {
+    val artifactoryUsername = System.getenv("ARTIFACTORY_PRIVATE_USERNAME") ?: project.findProperty("artifactoryUsername")
+    val artifactoryPassword = System.getenv("ARTIFACTORY_PRIVATE_PASSWORD") ?: project.findProperty("artifactoryPassword")
     maven {
         url = uri("https://repox.jfrog.io/repox/sonarsource")
+        if (artifactoryUsername is String && artifactoryPassword is String) {
+            credentials {
+                username = artifactoryUsername
+                password = artifactoryPassword
+            }
+        }
     }
     gradlePluginPortal()
 }
--- e2e/build.gradle.kts
+++ e2e/build.gradle.kts
@@ -28,8 +28,16 @@ dependencies {
 }
 
 repositories {
+    val artifactoryUsername = System.getenv("ARTIFACTORY_PRIVATE_USERNAME") ?: project.findProperty("artifactoryUsername")
+    val artifactoryPassword = System.getenv("ARTIFACTORY_PRIVATE_PASSWORD") ?: project.findProperty("artifactoryPassword")
     maven {
         url = uri("https://repox.jfrog.io/repox/sonarsource")
+        if (artifactoryUsername is String && artifactoryPassword is String) {
+            credentials {
+                username = artifactoryUsername
+                password = artifactoryPassword
+            }
+        }
     }
     mavenLocal()
 }
